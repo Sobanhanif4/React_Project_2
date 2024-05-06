d9c62bc45cfa6a6477a074a4bbcea6b02d248500
Soban Hanif <sobanatban4@gmail.com>
2024-05-06 17:14:15 +0500

    Contact_Page_React

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import { MdMessage } from "react-icons/md";
+import styles from "./Button.module.css"
+const Button = () => {
+  return (
+
+    
+    <button className={styles.primary_btn}>
+      <MdMessage />
+      Button
+      
+    </button>
+  )
+}
+
+export default Button
